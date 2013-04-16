4890aff6c0906bbba83fb242887be864b0ce58f3
Daniel Lago <dlagosuarez@gmail.com>
2013-04-16 19:17:54 +0200

    Create a sample mocha test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
-var should = require("should")
+var libpath = '../lib';
 
-describe('Array', function(){
-	describe('#indexOf()', function(){
-		it('should return -1 when the value is not present', function(){
-			[1,2,3].indexOf(5).should.equal(-1);
-			[1,2,3].indexOf(0).should.equal(-1);
+require('../src/mongo')
+require('../model/User')
+var assert = require('chai').assert
+var expect = require('chai').expect
+var should = require('chai').should
+
+describe('Mongo', function() {
+	var db = new Mongo(
+		'localhost',
+		'test',
+		new User()
+	);
+
+	beforeEach(function(done) {
+		db.clear();
+		done();
+	})
+
+	describe('all()', function() {
+		it('respond with all records', function(done){
+			db.all(function(data) {
+				var all = data;
+				expect(all).to.be.empty;
+				done();
+			});   
 		})
 	})
+
+	assert.instanceOf(db, Mongo, 'db is an instance of Mongo');
 })
